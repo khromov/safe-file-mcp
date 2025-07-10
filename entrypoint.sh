07b64bed3a2a68d98dd694983bd15646a98361c5
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-07-10 20:57:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
-# Entrypoint script for Coco MCP Docker container
-# Handles both stdio and HTTP transport modes
-#
-# Environment variables:
-#   MCP_TRANSPORT - Set to "stdio" for stdio mode, anything else defaults to HTTP mode
-#
-# Usage:
-#   This script is automatically used as the Docker entrypoint
-#   It determines the transport mode and starts the appropriate server
-#
-# IMPORTANT: In stdio mode, stdout is reserved for MCP protocol messages only
-# All log messages must go to stderr
-
 if [ "$MCP_TRANSPORT" = "stdio" ]; then
   # In stdio mode, all logs MUST go to stderr to avoid breaking the protocol
   exec node /opt/mcp-server/dist/index.js --stdio
